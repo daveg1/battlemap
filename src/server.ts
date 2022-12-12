aa1243d758966a4d6347e599e84a448ee33388d2
David Graham <d.graham8@rgu.ac.uk>
2022-12-12 19:19:08 +0000

    modern: fix view issue; update build steps

--- src/server.ts
+++ src/server.ts
@@ -7,15 +7,20 @@ import { __dirname } from './modules/__dirname.js'
 import { router } from './routes/index.js'
 import { connectDB } from './db/mongo.js'
 import { timestamp } from './modules/timestamp.js'
+import { IActiveUser } from './types/ActiveUser.js'
 
 const app = express()
+const baseDir = __dirname(import.meta.url)
 
 // Server variables
 app.set('port', process.env.PORT || 8080)
-app.set('views', path.join(__dirname(import.meta.url), 'views'))
+app.set('views', path.join(baseDir, 'views'))
 app.set('view engine', 'ejs')
 
 // Server middleware
+app.use(express.json())
+app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static(path.join(baseDir, 'public')))
 app.use(
 	session({
 		secret: 'secret-key',
@@ -23,17 +28,10 @@ app.use(
 		saveUninitialized: false,
 	}),
 )
-app.use(express.json())
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(express.static('public'))
-
-interface ActiveUser {
-	username: string
-}
 
 declare module 'express-session' {
 	interface SessionData {
-		activeUser: ActiveUser
+		activeUser: IActiveUser
 	}
 }
 
